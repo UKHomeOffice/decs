1702b017ffad8ceed4bd33b48bea5b413264be99
dap-protos <thomas.mearns@digital.homeoffice.gov.uk>
2022-06-30 14:59:07 +0100

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,16 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+
+  // Show and hide toggled content
+  // Where .multiple-choice uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+})
