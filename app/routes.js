80c407a25e93beea008d7588f2d5e66a78716b31
dap-protos <thomas.mearns@digital.homeoffice.gov.uk>
2022-07-29 17:40:13 +0100

    PSU journey

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-
+// Logs saved data in the terminal
 router.use((req, res, next) => {
       const log = {
         method: req.method,
@@ -32,22 +32,58 @@ router.post('/juggling-balls-answer', function (req, res) {
 })
 
 
-// Run this code when a form is submitted to 'triage-decision'
+// Run this code when a form is submitted to '/triage-decision' form on triage.html
 router.post('/triage-decision', function (req, res) {
 
-  // Make a variable and give it the value from 'how-many-balls'
+  // Make a variable and give it the value from 'serious' radios
   var triageDecision = req.session.data['serious']
 
-  // Check whether the variable matches a condition
+  // Check whether the variable matches a condition.
   if (triageDecision == "yes"){
-    // Send user to next page
-    res.redirect('/psu-v1/outcome')
+    // Send user to categories page
+    res.redirect('/psu-v1/is-serious-categories')
+  } else if (triageDecision == "no"){
+    // Send user to page that asks to recategorise
+    res.redirect('/psu-v1/reclassify-nonserious-complaint-category')
   } else {
-    // Send user to ineligible page
-    res.redirect('/psu-v1/reclassify')
+    // Send user to page that asks whether the non-DECS team needs to look at it
+    res.redirect('/psu-v1/index')
   }
 
 })
 
+// Run this code when a form is submitted to '/team-not-on-decs' on non-decs.html
+router.post('/team-not-on-decs', function (req, res) {
+
+  // Make a variable and give it the value from 'non-decs-team' radios
+  var nonDecs = req.session.data['non-decs-team']
+
+  // Check whether the variable matches a condition.
+  if (nonDecs == "yes"){
+    // Send user to dashboard??
+    res.redirect('/psu-v1/index')
+  } else {
+    // Send user to complaint type page
+    res.redirect('/psu-v1/reclassify-nonserious-complaint-type')
+  }
+
+})
+
+// Run this code when a form is submitted to '/is-serious-recategorise' on is-serious-categories.html
+router.post('/is-serious-recategorise', function (req, res) {
+
+  // Make a variable and give it the value from 'non-decs-team' radios
+  var recategoriseSerious = req.session.data['recategorise-serious']
+
+  // Check whether the variable matches a condition.
+  if (recategoriseSerious == "yes"){
+    // Send user to dashboard??
+    res.redirect('/psu-v1/reclassify-serious-complaint-type')
+  } else {
+    // Send user to complaint type page
+    res.redirect('/psu-v1/index')
+  }
+
+})
 
 module.exports = router
